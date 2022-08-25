710cc9dc1fb4cb5a78279fea1c6df9620db2b6f6
Nikolay Kolomyytsev <kolya.ya@gmail.com>
2022-08-25 21:16:46 +0200

    add styling to console output

--- BasicJS-HW/loopsAndArrowFunctions/main.js
+++ BasicJS-HW/loopsAndArrowFunctions/main.js
@@ -1,12 +1,14 @@
 const firstLevel = () => {
     let evenNums = ""
+    console.group("Even numbers from 2 to 10")
     for (let i = 2; i <= 10; i++) {
         if (i % 2 === 0) {
             evenNums += `${i} `
-            console.log(`Even number: ${i}`)
+            console.log(`Even number: %c${i}`, "color: crimson; background: lightblue; padding: 3px 5px;")
         }
 
     }
+    console.groupEnd()
     alert(`Even numbers from 2 to 10: ${evenNums}`)
     
     let i = 0
